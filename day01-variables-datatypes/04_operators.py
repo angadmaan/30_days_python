1f982e234e74238d4a993a343e8ef3fb67de5e67
angadmaan <angadmaancse@gmail.com>
2026-07-08 13:25:56 +0530

    day 1

--- day01-variables-datatypes/04_operators.py
+++ day01-variables-datatypes/04_operators.py
@@ -85,5 +85,24 @@ print("Logical AND:", p and q)
 print("Logical OR:", p or q)
 print("Logical NOT:", not p)
 
+print("Truth Table for Logical Operators:")
 
+print("Truth table for 'OR' operator:")
+
+print("true or true =", True or True)
+print("true or false =", True or False)
+print("false or true =", False or True)
+print("false or false =", False or False)
+
+print("Truth table for 'AND' operator:")
+
+print("true and true =", True and True)
+print("true and false =", True and False)
+print("false and true =", False and True)
+print("false and false =", False and False)
+
+print("Truth table for 'NOT' operator:")
+
+print("not true =", not True)
+print("not false =", not False)
 
