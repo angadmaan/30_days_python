a3ec22f3f54f4355c35bee1dd7a2b101d529390f
angadmaan <angadmaancse@gmail.com>
2026-07-15 23:22:26 +0530

    day 3

--- day03-dicts-conditionals/conditionals.py
+++ day03-dicts-conditionals/conditionals.py
@@ -15,8 +15,39 @@ print("Thank you for using the driving license eligibility checker") # This prin
 
 # Relational Operators in python 
 
+# Relational operators are used to compare two values.
+# They return True if the comparison is true, and False if the comparison is false.
+
+# ==  Equal to
+# !=  Not equal to
+# >   Greater than
+# <   Less than
+# >=  Greater than or equal to
+# <=  Less than or equal to
 
 
+# Logical Operators in python
+
+# Logical operators are used to combine two or more conditions.
+
+# and : Returns True if both conditions are true.
+# or  : Returns True if at least one condition is true.
+# not : Returns the opposite of the condition.
 
 
+# elif Clause
+
+# if (condition1):
+
+# elif (condition2):
+
+# elif(condition3):
+
+# else:
+
+# this ladder will stop once a condition in an if or elif is met.
+
+
+# 1. There can be any number of elif statements.
+# 2. Last else is executed only if all the conditions inside elifs fail.
 
